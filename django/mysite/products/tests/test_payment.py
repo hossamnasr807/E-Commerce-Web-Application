494f189fb9f0b7e84a76c4f53544238583b4ea0d
Hossam Nasr Mohamed <84002927+hossamnasr807@users.noreply.github.com>
2024-12-24 11:22:43 +0200

    Add files via upload

---
+++ django/mysite/products/tests/test_payment.py
@@ -0,0 +1,24 @@
+import requests
+from django.test import TestCase
+
+class PaymentTestCase(TestCase):
+    def test_process_payment(self):
+        payment_data = {
+            'method': 'fawry',
+            'amount': 400.00,
+            'order_id': 8
+        }
+
+        response = requests.post('http://127.0.0.1:8000/api/process-payment/', json=payment_data)
+        self.assertEqual(response.status_code, 200)
+        
+        # Check that the 'message' key exists and has the correct value
+        self.assertIn('message', response.json())
+        self.assertEqual(response.json()['message'], 'Payment processed successfully!')
+
+        # Check the structure of the 'payment' key
+        self.assertIn('payment', response.json())
+        payment_info = response.json()['payment']
+        self.assertEqual(payment_info['order'], 8)
+        self.assertEqual(payment_info['amount'], '400.00')
+        self.assertEqual(payment_info['method'], 'fawry')
---
+++ django/mysite/products/tests/test_views.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+
+class HomeViewTestCase(TestCase):
+    def test_home_page(self):
+        response = self.client.get(reverse('home'))  # Adjust to your URL name
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, '<h1>Welcome to the E-Commerce Site</h1>')
+
+    def test_home_view(self):
+        response = self.client.get(reverse('home'))  # Adjust to your URL name
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, 'Welcome to the E-Commerce Site')
+
+    def test_about_page(self):
+        response = self.client.get(reverse('about'))  # Adjust to your URL name
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, " the About page ")
