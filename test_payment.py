96271ee326667fd5b8f54fd6fabb9e9b14a7cd67
Hossam Nasr Mohamed <84002927+hossamnasr807@users.noreply.github.com>
2024-12-24 10:48:29 +0200

    Update test_payment.py

--- test_payment.py
+++ test_payment.py
@@ -6,9 +6,19 @@ class PaymentTestCase(TestCase):
         payment_data = {
             'method': 'fawry',
             'amount': 400.00,
-            'order_id': 5
+            'order_id': 8
         }
 
         response = requests.post('http://127.0.0.1:8000/api/process-payment/', json=payment_data)
         self.assertEqual(response.status_code, 200)
-        self.assertIn('payment_status', response.json())  # Ensure payment status is returned
+        
+        # Check that the 'message' key exists and has the correct value
+        self.assertIn('message', response.json())
+        self.assertEqual(response.json()['message'], 'Payment processed successfully!')
+
+        # Check the structure of the 'payment' key
+        self.assertIn('payment', response.json())
+        payment_info = response.json()['payment']
+        self.assertEqual(payment_info['order'], 8)
+        self.assertEqual(payment_info['amount'], '400.00')
+        self.assertEqual(payment_info['method'], 'fawry')
